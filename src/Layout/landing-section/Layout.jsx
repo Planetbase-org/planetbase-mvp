1bb7c420923a4b75c1f3e95b7f46d151f98e5f1b
Heytemisan <temyicy@gmail.com>
2022-08-09 18:06:03 +0100

    feat: created landing-section layout

---
+++ src/Layout/landing-section/Layout.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../../components/NavBar/Navbar'
+im
+
+function Layout() {
+    return (
+        <>
+            <Navbar/>
+        </>
+    )
+}
+
+export default Layout
