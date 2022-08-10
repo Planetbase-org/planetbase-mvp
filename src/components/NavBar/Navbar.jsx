78ca296656e8b400279bd483e9fa1fdfcc2533af
Heytemisan <temyicy@gmail.com>
2022-08-10 18:36:05 +0100

    chore: code cleanup

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -15,15 +15,13 @@ function Navbar() {
                 className="hamburger"
                 onClick={() => {
                     setIsNavExpanded(!isNavExpanded);
-                }}
-            >
+                }}>
                 {/* icon from Heroicons.com */}
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
                     className="h-5 w-5"
                     viewBox="0 0 20 20"
-                    fill="white"
-                >
+                    fill="white">
                     <path
                         fillRule="evenodd"
                         d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
@@ -58,6 +56,7 @@ function Navbar() {
                         </Link>
                     </li>
                 </ul>
+                <button className="loginButton">Login</button>
             </div>
         </nav>
     );
