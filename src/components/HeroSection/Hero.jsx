5549ea435b9b5018b8a13df0ad5d8b77ed241942
Heytemisan <temyicy@gmail.com>
2022-08-10 21:43:37 +0100

    feat: adjusted button size

--- src/components/HeroSection/Hero.jsx
+++ src/components/HeroSection/Hero.jsx
@@ -5,7 +5,7 @@ function HeroSection() {
     return (
         <div className='hero-section'>
             <h1>Welcome To Planetbase</h1>
-            <p>A Place where events are created and sponsors can come <br /> in to Market their brands/products.<a href="#">Learn more</a></p>
+            <p>A Place where events are created and sponsors can come <br /> in to Market their brands/products.<a href="#" className="learn-more">Learn more</a></p>
             <h4>Who Are You?</h4>
             <button>SPONSOR</button>
             <button>ORGANISER</button>
