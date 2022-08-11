5bb19a0b949dba32c5bdea2511e932bfe06feb0c
Heytemisan <temyicy@gmail.com>
2022-08-11 16:52:26 +0100

    chore: code refactored

--- src/components/HeroSection/Hero.jsx
+++ src/components/HeroSection/Hero.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import "./Hero.css"
 
 function HeroSection() {
     return (
         <div className='hero-section'>
-            <h1>Connecting Corporate sponsors
+            <h1>Connecting Corporate sponsors <br />
               & Event Organisers</h1>
             <p>‘’Reach your target audience, surpass your revenue goals’’.</p>
             <h4>Who Are You?</h4>
-            <button>SPONSOR</button>
-            <button>ORGANISER</button>
+            <Link to="/sponsor">SPONSOR</Link>
+            <Link to="/organiser">ORGANISER</Link>
         </div>  
     )
 }
