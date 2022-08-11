68fef61fd7584502a4450d7d59855211558a05e1
Heytemisan <temyicy@gmail.com>
2022-08-11 16:34:13 +0100

    feat: adjusted the texts

--- src/components/HeroSection/Hero.jsx
+++ src/components/HeroSection/Hero.jsx
@@ -4,12 +4,13 @@ import "./Hero.css"
 function HeroSection() {
     return (
         <div className='hero-section'>
-            <h1>Welcome To Planetbase</h1>
-            <p>A Place where events are created and sponsors can come <br /> in to Market their brands/products.<a href="#" className="learn-more">Learn more</a></p>
+            <h1>Connecting Corporate sponsors
+              & Event Organisers</h1>
+            <p>‘’Reach your target audience, surpass your revenue goals’’.</p>
             <h4>Who Are You?</h4>
             <button>SPONSOR</button>
             <button>ORGANISER</button>
-        </div>
+        </div>  
     )
 }
 
