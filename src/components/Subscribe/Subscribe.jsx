df52aa026ae3014d4da74263a12bad373ea6fcef
Heytemisan <temyicy@gmail.com>
2022-08-11 19:20:20 +0100

    chore: adjusted subscribe input botton

--- src/components/Subscribe/Subscribe.jsx
+++ src/components/Subscribe/Subscribe.jsx
@@ -19,7 +19,7 @@ function Subscribe() {
             <input type="text" placeholder='Enter email address' className='subscribe-email-input'/>
             <button className='subscribe-btn'>Subscribe</button>
           </form>
-          <p>By signing up, you agree to our Terms of Service and Privacy Policy. You will receive offers from Wimdu and agree that we may share your hashed email address with third parties for the purpose of better tailoring advertising to your needs.</p>
+          <p className='subscribe-message-text'>By signing up, you agree to our Terms of Service and Privacy Policy. You will receive offers from Wimdu and agree that we may share your hashed email address with third parties for the purpose of better tailoring advertising to your needs.</p>
         </div>
       </div>
     </section>
