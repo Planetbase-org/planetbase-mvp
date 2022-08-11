c8a09732b87e36736e09f30f094e3e062200f9d1
Heytemisan <temyicy@gmail.com>
2022-08-11 18:58:08 +0100

    feat: styled subscribe section

--- src/components/Subscribe/Subscribe.jsx
+++ src/components/Subscribe/Subscribe.jsx
@@ -4,22 +4,25 @@ import "./Subscribe.css"
 
 function Subscribe() {
   return (
-    <div className="subscribe">
-      <p>
+    <section className="subscribe">
+      <div className="subscribe-header">
         <img src={Planetbase} alt="" />
-      </p>
-      <div>
-        <h4>Moving your events online really opens up the esteem of supporting it </h4>
-        <p>Become a subscriber and receive great tips on eventl regulation updates sent to your inbox!</p>
+        <h4>PLANETBASE</h4>
       </div>
-      <div>
-        <form>
-          <input type="text" placeholder='Enter email address' />
-          <button>Subscribe</button>
-        </form>
-        <p>By signing up, you agree to our Terms of Service and Privacy Policy. You will receive offers from Wimdu and agree that we may share your hashed email address with third parties for the purpose of better tailoring advertising to your needs.</p>
+      <div className="subscribe-message">
+        <div>
+          <h4>Moving your events online really opens up the esteem of supporting it </h4>
+          <p>Become a subscriber and receive great tips on eventl regulation updates sent to your inbox!</p>
+        </div>
+        <div><br />
+          <form>
+            <input type="text" placeholder='Enter email address' className='subscribe-email-input'/>
+            <button className='subscribe-btn'>Subscribe</button>
+          </form>
+          <p>By signing up, you agree to our Terms of Service and Privacy Policy. You will receive offers from Wimdu and agree that we may share your hashed email address with third parties for the purpose of better tailoring advertising to your needs.</p>
+        </div>
       </div>
-    </div>
+    </section>
   )
 }
 
