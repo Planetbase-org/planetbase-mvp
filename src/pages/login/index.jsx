02d8e9f43f9e0db828b5f37c8f3690f53ffc4f6d
Heytemisan <temyicy@gmail.com>
2022-08-12 16:38:08 +0100

    feat: code refactored

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -6,7 +6,7 @@ function Login() {
     return (
         <Layout>
             <div className='input-container'>
-                <input type="text" name='email' id='email' placeholder='Email Address' />
+                <input type="email" name='email' id='email' placeholder='Email Address' />
                 <input type="password" name='password' id='password' placeholder='Password' />
                 <p>Forgot Password ?</p>
                 <button type="submit" className="input-button">Sign In</button>
