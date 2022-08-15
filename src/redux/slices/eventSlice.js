34be3c0c1cc8c6989a17f9b7eb69ddd10c6b2c51
Heytemisan <temyicy@gmail.com>
2022-08-15 16:49:48 +0100

    feat: eventSlice

--- src/redux/slices/eventSlice.js
+++ src/redux/slices/eventSlice.js
@@ -17,8 +17,20 @@ const eventSlice = createSlice({
     name: "event",
     initialState,
     reducers: {
-        setEvent: (state, action) => { 
-            state.
+        setEvent: (state, action) => {
+            state.title = action.payload;
+            state.categories = action.payload;
+            state.event = action.payload;
+            state.tags = action.payload;
+            state.guest = action.payload;
+            state.date = action.payload;
+            state.status = true;
+            state.price = action.payload;
+            state.description = action.payload;
+            state.sponsorship = action.payload;
         }
     }
-})
+});
+
+export default eventSlice.reducer;
+export const { setEvent } = eventSlice.actions;
