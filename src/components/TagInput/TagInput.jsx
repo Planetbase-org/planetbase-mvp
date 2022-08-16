e25c1627707d891e953085e0c4c4796e4cc8d6fa
Heytemisan <temyicy@gmail.com>
2022-08-16 11:36:18 +0100

    feat: code cleanup

--- src/components/TagInput/TagInput.jsx
+++ src/components/TagInput/TagInput.jsx
@@ -4,45 +4,48 @@ import "./TagInput.css"
 
 function TagsInput({ handleChange, value }) {
 
-    const [tags, setTags] = useState([])
-    const storedValues = localStorage.getItem('event')
-    if (!storedValues) return {
-        tags: ""
-    }
-
-    return JSON.parse(storedValues);
+    function getFormValue() {
+        if (!storedValues) return {
+            tag: [],
+        }
+    
+        return JSON.parse(storedValues);
     };
 
-function handleKeyDown(e) {
-    if (e.key !== 'Enter') return
-    const value = e.target.value
-    if (!value.trim()) return
-    setTags([...tags, value])
-    e.target.value = ''
-}
+    const [tags, setTags] = useState(getFormValue);
+    const storedValues = localStorage.getItem('event');
 
-function removeTag(index) {
-    setTags(tags.filter((el, i) => i !== index))
-}
 
-return (
-    <div className="tags-input-container">
-        {tags.map((tag, index) => (
-            <div className="tag-item" key={index}>
-                <span className="text">{tag}</span>
-                <span className="close" onClick={() => removeTag(index)}>&times;</span>
-            </div>
-        ))}
-        <input
-            onKeyDown={handleKeyDown}
-            type="text"
-            className="tags-input"
-            placeholder="Type something"
-            onChange={handleChange}
-        // value={value.tags}
-        />
-    </div>
-)
+    function handleKeyDown(e) {
+        if (e.key !== 'Enter') return
+        const value = e.target.value
+        if (!value.trim()) return
+        setTags([...tags, value])
+        e.target.value = ''
+    }
+
+    function removeTag(index) {
+        setTags(tags.filter((el, i) => i !== index))
+    }
+
+    return (
+        <div className="tags-input-container">
+            {tags.map((tag, index) => (
+                <div className="tag-item" key={index}>
+                    <span className="text">{tag}</span>
+                    <span className="close" onClick={() => removeTag(index)}>&times;</span>
+                </div>
+            ))}
+            <input
+                onKeyDown={handleKeyDown}
+                type="text"
+                className="tags-input"
+                placeholder="Type something"
+                onChange={handleChange}
+            // value={value.tags}
+            />
+        </div>
+    )
 }
 
 export default TagsInput
