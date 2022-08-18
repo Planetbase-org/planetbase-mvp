5dc549fa8e8ca56d78e551e5cab422db6aecc4e0
Heytemisan <temyicy@gmail.com>
2022-08-18 21:49:20 +0100

    feat: sponsorship-search filter

--- src/pages/sponsor/index.jsx
+++ src/pages/sponsor/index.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { FiSearch } from 'react-icons/fi';
+import "./style.css"
 
 function Sponsor() {
   return (
-    <div>Sponsor</div>
+      <div>
+      <h1>Discover Events Where Your Target Audience Is</h1>
+      <div className="search-events sponsorship-search">
+          <span>
+            <FiSearch />
+          </span>
+          <input type="text" placeholder="Search for your event" />
+        </div>
+    </div>
   )
 }
 
