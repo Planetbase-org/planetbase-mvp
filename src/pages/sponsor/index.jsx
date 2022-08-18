14be7e09b4359f5b1eb5cd0afb33376ac45043dd
Heytemisan <temyicy@gmail.com>
2022-08-18 21:57:12 +0100

    feat: created grid layout

--- src/pages/sponsor/index.jsx
+++ src/pages/sponsor/index.jsx
@@ -1,19 +1,48 @@
-import React from 'react'
-import { FiSearch } from 'react-icons/fi';
-import "./style.css"
+import React from "react";
+import { FiSearch } from "react-icons/fi";
+import "./style.css";
 
 function Sponsor() {
   return (
-      <div>
+    <div>
       <h1>Discover Events Where Your Target Audience Is</h1>
       <div className="search-events sponsorship-search">
-          <span>
-            <FiSearch />
-          </span>
-          <input type="text" placeholder="Search for your event" />
+        <span>
+          <FiSearch />
+        </span>
+        <input type="text" placeholder="Search for your event" />
+      </div>
+      <div>
+        <a>Listed Events</a>
+      </div>
+      <div className="sponsorships">
+        <div class="sponsorships-layout">
+          <div>Hello</div>
+          <div>Hello</div>
+          <div>Hello</div>
+          <div>Hello</div>
         </div>
+        <div class="sponsorships-layout">
+          <div>Hello</div>
+          <div>Hello</div>
+          <div>Hello</div>
+          <div>Hello</div>
+        </div>
+        <div class="sponsorships-layout">
+          <div>Hello</div>
+          <div>Hello</div>
+          <div>Hello</div>
+          <div>Hello</div>
+        </div>
+        <div class="sponsorships-layout">
+          <div>Hello</div>
+          <div>Hello</div>
+          <div>Hello</div>
+          <div>Hello</div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Sponsor
+export default Sponsor;
