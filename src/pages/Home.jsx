1cfe89fb22fff6b76df5c83f8ac5ada565c95251
Heytemisan <temyicy@gmail.com>
2022-08-19 21:16:09 +0100

    fix: code cleanup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import HeroSection from '../components/HeroSection/Hero';
 import Layout from '../layouts/landing-section';
 import Subscribe from '../components/Subscribe/Subscribe';
+import ModalVideo from '../components/Video/Video';
 
 function Home() {
     return (
         <Layout>
             <HeroSection />
-                
+                <ModalVideo />
             <Subscribe/>
         </Layout>
     )
