3c25c5ed227e90bea255aa5f7950f38c2a45d1e5
Heytemisan <temyicy@gmail.com>
2022-08-21 22:44:48 +0100

    feat: style refactored

--- src/components/SignUpComponent/SignUpComponent.jsx
+++ src/components/SignUpComponent/SignUpComponent.jsx
@@ -1,31 +1,37 @@
-import React from 'react';
-import "./SignUpComponent.css"
-import LinkedIn from "./../../assets/linkedin.svg";
-import Google from "./../../assets/google.svg";
-import Facebook from "./../../assets/facebook.svg";
+import React from 'react'
+import './SignUpComponent.css'
+import LinkedIn from './../../assets/linkedin.svg'
+import Google from './../../assets/google.svg'
+import Facebook from './../../assets/facebook.svg'
 
-const SignUpComponent = ({ header, children}) => {
-    return (
-        <div className='sign-up'>
-            <h1>{header}</h1>
-            
-            {children}
-            <div className="sign-up-section">
-                <div className='sign-up-link'>
-                    <img src={Facebook} alt="facebook" />
-                    <p>Facebook</p>
-                </div>
-                <div className='sign-up-link'>
-                    <img src={Google} alt="google" />
-                    <p>Google</p>
-                </div>
-                <div className='sign-up-link'>
-                    <img src={LinkedIn} alt="linkedin" />
-                    <p>LinkedIn</p>
-                </div>
-            </div>
+const SignUpComponent = ({ header, children }) => {
+  return (
+    <div className='sign-up'>
+      <h1>{header}</h1>
+
+      {children}
+      <div className='sign-up-section'>
+        <div className='sign-up-link'>
+          <a href='#'>
+            <img src={Facebook} alt='facebook' />
+            <p>Facebook</p>
+          </a>
         </div>
-    )
+        <div className='sign-up-link'>
+          <a href='#'>
+            <img src={Google} alt='google' />
+            <p>Google</p>
+          </a>
+        </div>
+        <div className='sign-up-link'>
+          <a href='#'>
+            <img src={LinkedIn} alt='linkedin' />
+            <p>LinkedIn</p>
+          </a>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default SignUpComponent
