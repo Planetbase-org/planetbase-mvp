b015ee6d8eccb95474e566b4e830dfd6fe7a7748
Heytemisan <temyicy@gmail.com>
2022-08-22 21:40:03 +0100

    feat: code cleanup

--- src/components/NavBar/MenuNav.jsx
+++ src/components/NavBar/MenuNav.jsx
@@ -1,84 +1,86 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import "./Navbar.css";
-import Planetbase from "../../assets/planetbase.svg";
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import './Navbar.css'
+import Planetbase from '../../assets/planetbase.svg'
 
-function MenuNav() {
-    const [isNavExpanded, setIsNavExpanded] = useState(false);
+function MenuNav () {
+  const [isNavExpanded, setIsNavExpanded] = useState(false)
 
-    return (
-        <nav className="navigation">
-            <Link to="/" className="brand-name">
-                <img src={Planetbase} alt="planetbase" />
+  return (
+    <nav className='navigation'>
+      <Link to='/' className='brand-name'>
+        <img src={Planetbase} alt='planetbase' />
+      </Link>
+      <button
+        className='hamburger'
+        onClick={() => {
+          setIsNavExpanded(!isNavExpanded)
+        }}
+      >
+        {/* icon from Heroicons.com */}
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          className='h-5 w-5'
+          viewBox='0 0 20 20'
+          fill='white'
+        >
+          <path
+            fillRule='evenodd'
+            d='M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z'
+            clipRule='evenodd'
+          />
+        </svg>
+      </button>
+      <div
+        className={
+          isNavExpanded ? 'navigation-menu expanded' : 'navigation-menu'
+        }
+      >
+        <ul>
+          <li>
+            <Link to='/products' activeclassname='active'>
+              Product
             </Link>
-            <button
-                className="hamburger"
-                onClick={() => {
-                    setIsNavExpanded(!isNavExpanded);
-                }}
-            >
-                {/* icon from Heroicons.com */}
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5"
-                    viewBox="0 0 20 20"
-                    fill="white"
-                >
-                    <path
-                        fillRule="evenodd"
-                        d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
-                        clipRule="evenodd"
-                    />
-                </svg>
-            </button>
-            <div
-                className={
-                    isNavExpanded ? "navigation-menu expanded" : "navigation-menu"
-                }
-            >
-                <ul>
-                    <li>
-                        <Link to="/products" activeclassname="active">
-                            Product
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/solutions" activeclassname="active">
-                            Solutions
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/resources" activeclassname="active">
-                            Resources
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/company" activeclassname="active">
-                            Company
-                        </Link>
-                    </li>
-                    <div>
-                        <li className="login">
-                            <Link to="/login" activeclassname="active">
-                                Login
-                            </Link>
-                        </li>
-                        <li>
-                            <button className="loginBtnMobile">Create Events</button>
-                        </li>
-                    </div>
-                </ul>
-                <div className="createEvents">
-                    <li>
-                        <Link to="/login" activeclassname="active">
-                            Login
-                        </Link>
-                    </li>
-                    <button className="loginButton">Create Events</button>
-                </div>
-            </div>
-        </nav>
-    );
+          </li>
+          <li>
+            <Link to='/solutions' activeclassname='active'>
+              Solutions
+            </Link>
+          </li>
+          <li>
+            <Link to='/resources' activeclassname='active'>
+              Resources
+            </Link>
+          </li>
+          <li>
+            <Link to='/company' activeclassname='active'>
+              Company
+            </Link>
+          </li>
+          <div>
+            <li className='login'>
+              <Link to='/login' activeclassname='active'>
+                Login
+              </Link>
+            </li>
+            <li>
+              <button className='loginBtnMobile'>Create Events</button>
+            </li>
+          </div>
+        </ul>
+        <div className='createEvents'>
+          <li>
+            <Link to='/login' activeclassname='active'>
+              Login
+            </Link>
+          </li>
+          <Link to="/create-event">
+            <button className='loginButton' type="button">Create Events</button>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  )
 }
 
-export default MenuNav;
+export default MenuNav
--- src/pages/dashboard/edit.jsx
+++ src/pages/dashboard/edit.jsx
@@ -1,76 +1,76 @@
-import React, { useState, useEffect } from "react";
-import EventLayout from "../../layouts/events-layout";
-import { useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { events} from "../../redux/eventSlice";
+import React, { useState, useEffect } from 'react'
+import EventLayout from '../../layouts/events-layout'
+import { useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { events } from '../../redux/eventSlice'
 
-function EditEvent() {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
+function EditEvent () {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
 
-  function getFormValues() {
-    const storedValues = localStorage.getItem("events");
+  function getFormValues () {
+    const storedValues = localStorage.getItem('events')
     if (!storedValues)
       return {
         title: [],
-        categories: "",
-        events: "",
-        guest: "",
-        date: "",
+        categories: '',
+        events: '',
+        guest: '',
+        date: '',
         status1: false,
         status2: false,
-        price: "",
-        description: "",
-        sponsorship: "",
-      };
-    return JSON.parse(storedValues);
+        price: '',
+        description: '',
+        sponsorship: ''
+      }
+    return JSON.parse(storedValues)
   }
 
-  const [value, setValue] = useState(getFormValues);
-  const [tags, setTags] = useState([]);
+  const [value, setValue] = useState(getFormValues)
+  const [tags, setTags] = useState([])
 
   useEffect(() => {
-    localStorage.setItem("event", JSON.stringify(value, null, 2));
-  }, [value, dispatch, setValue]);
+    localStorage.setItem('event', JSON.stringify(value, null, 2))
+  }, [value, dispatch, setValue])
 
-  function onSubmit(e) {
-    e.preventDefault();
-    dispatch(events(value));
-    navigate("/organizer");
+  function onSubmit (e) {
+    e.preventDefault()
+    dispatch(events(value))
+    navigate('/organizer')
   }
 
-  const onChange = (e) => {
-    setValue((prevValues) => ({
+  const onChange = e => {
+    setValue(prevValues => ({
       ...prevValues,
-      [e.target.name]: e.target.value,
-    }));
-  };
-
-  function handleKeyDown(e) {
-    if (e.key !== "Enter") return;
-    const value = e.target.value;
-    if (!value.trim()) return;
-    setTags([...tags, value]);
-    e.target.value = "";
+      [e.target.name]: e.target.value
+    }))
   }
 
-  function removeTag(index) {
-    setTags(tags.filter((el, i) => i !== index));
+  function handleKeyDown (e) {
+    if (e.key !== 'Enter') return
+    const value = e.target.value
+    if (!value.trim()) return
+    setTags([...tags, value])
+    e.target.value = ''
+  }
+
+  function removeTag (index) {
+    setTags(tags.filter((el, i) => i !== index))
   }
 
   return (
     <EventLayout>
-      <div className="event-form">
+      <div className='event-form'>
         <h1>List Your Upcoming Event</h1>
         <form onSubmit={onSubmit}>
-          <div className="form">
-            <div className="form-control">
+          <div className='form'>
+            <div className='form-control'>
               <div>
                 <p>Event title</p>
                 <input
-                  type="text"
-                  name="title"
-                  id="title"
+                  type='text'
+                  name='title'
+                  id='title'
                   onChange={onChange}
                   value={value.title}
                 />
@@ -78,64 +78,64 @@ function EditEvent() {
               <div>
                 <p>Event Organizer/host</p>
                 <select
-                  name="file"
-                  id=""
-                  className="select-events"
+                  name='file'
+                  id=''
+                  className='select-events'
                   onChange={onChange}
                 >
                   <option value={value.categories}>{value.categories}</option>
-                  <option value="events">Events</option>
-                  <option value="projects">Projects</option>
+                  <option value='events'>Events</option>
+                  <option value='projects'>Projects</option>
                 </select>
               </div>
             </div>
-            <div className="form-control">
+            <div className='form-control'>
               <div>
                 <p>Event type/category</p>
                 <input
-                  type="text"
-                  name="events"
-                  id="events"
+                  type='text'
+                  name='events'
+                  id='events'
                   onChange={onChange}
                   value={value.events}
                 />
               </div>
-              <div className="tags-input-container">
+              <div className='tags-input-container'>
                 {tags.map((tag, index) => (
-                  <div className="tag-item" key={index}>
-                    <span className="text">{tag}</span>
-                    <span className="close" onClick={() => removeTag(index)}>
+                  <div className='tag-item' key={index}>
+                    <span className='text'>{tag}</span>
+                    <span className='close' onClick={() => removeTag(index)}>
                       &times;
                     </span>
                   </div>
                 ))}
                 <input
                   onKeyDown={handleKeyDown}
-                  type="text"
-                  className="tags-input"
-                  placeholder="Type something"
+                  type='text'
+                  className='tags-input'
+                  placeholder='Type something'
                 />
               </div>
             </div>
-            <div className="form-control">
-              <div className="form-details">
-                <div className="details">
+            <div className='form-control'>
+              <div className='form-details'>
+                <div className='details'>
                   <p>Expected Number of guests</p>
                   <input
-                    type="number"
-                    name="number"
-                    id="number"
+                    type='number'
+                    name='number'
+                    id='number'
                     onChange={onChange}
                     value={value.number}
                   />
                 </div>
               </div>
-              <div className="form-checkbox">
+              <div className='form-checkbox'>
                 <div>
                   <input
-                    type="radio"
-                    name="status1"
-                    id="status1"
+                    type='radio'
+                    name='status1'
+                    id='status1'
                     onChange={onChange}
                     checked={value.status1}
                   />
@@ -143,9 +143,9 @@ function EditEvent() {
                 </div>
                 <div>
                   <input
-                    type="radio"
-                    name="status2"
-                    id="status2"
+                    type='radio'
+                    name='status2'
+                    id='status2'
                     onChange={onChange}
                     checked={value.status2}
                   />
@@ -153,25 +153,25 @@ function EditEvent() {
                 </div>
               </div>
             </div>
-            <div className="form-control">
+            <div className='form-control'>
               <div>
-                <div className="details">
+                <div className='details'>
                   <p>Scheduled date</p>
                   <input
-                    type="date"
-                    id="date"
-                    name="date"
+                    type='date'
+                    id='date'
+                    name='date'
                     onChange={onChange}
                     value={value.date}
                   />
                 </div>
-                <div className="price">
+                <div className='price'>
                   <p>Price</p>
                   <input
-                    type="number"
-                    placeholder="₦"
-                    id="price"
-                    name="price"
+                    type='number'
+                    placeholder='₦'
+                    id='price'
+                    name='price'
                     onChange={onChange}
                     value={value.price}
                   />
@@ -181,33 +181,33 @@ function EditEvent() {
           </div>
 
           <div>
-            <div className="description">
+            <div className='description'>
               <h1>Event Description</h1>
               <textarea
-                name="description"
-                id="description"
-                cols="30"
-                rows="10"
-                placeholder="Briefly describe what your event is all about"
+                name='description'
+                id='description'
+                cols='30'
+                rows='10'
+                placeholder='Briefly describe what your event is all about'
                 value={value.description}
                 onChange={onChange}
               ></textarea>
             </div>
-            <div className="description">
+            <div className='description'>
               <h1>Sponsorship Package</h1>
               <textarea
-                name="sponsorship"
-                id="sponsorship"
-                cols="30"
-                rows="10"
+                name='sponsorship'
+                id='sponsorship'
+                cols='30'
+                rows='10'
                 placeholder="Provide sponsor's benefit here"
                 value={value.sponsorship}
                 onChange={onChange}
               ></textarea>
             </div>
-            <div className="form-upload">
-              <div className="upload"></div>
-              <div className="upload-instruction">
+            <div className='form-upload'>
+              <div className='upload'></div>
+              <div className='upload-instruction'>
                 <p>
                   Give your event a visual expression to make it more
                   noticeable.
@@ -219,15 +219,15 @@ function EditEvent() {
               </div>
             </div>
           </div>
-          <div className="update-event">
-            <button type="submit" className="btn-primary">
+          <div className='update-event'>
+            <button type='button' className='btn-primary'>
               <span>Save and continue</span>
             </button>
           </div>
         </form>
       </div>
     </EventLayout>
-  );
+  )
 }
 
-export default EditEvent;
+export default EditEvent
