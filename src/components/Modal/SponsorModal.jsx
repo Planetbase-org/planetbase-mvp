c7c304e294433b31261d064d0f3fce52938a498d
Heytemisan <temyicy@gmail.com>
2022-08-23 19:35:35 +0100

    feat: styling Sponsor modal

--- src/components/Modal/SponsorModal.jsx
+++ src/components/Modal/SponsorModal.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
+import "./Modal.css"
 
 function SponsorModal () {
     return <div>
-        <img src="" alt="" />
-        <h4>B2B Ecommere conference</h4>
-        <p>3801 Richardson, California 62639</p>
+        <img src="https://res.cloudinary.com/dputu7z0u/image/upload/v1660863670/Rectangle_31_ad96pw.svg" alt="sponsor image" />
+        <h4>B2B E-Commerce conference</h4>
+        <p className="address">3801 Richardson, California 62639</p>
         <h5>Description</h5>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit voluptatum odit nisi eos sequi sunt magnam! Ab veniam inventore voluptas molestias amet non doloribus sint excepturi, modi, cumque delectus doloremque!</p>
         <p>DATE: 2022/9/12/9am</p>
