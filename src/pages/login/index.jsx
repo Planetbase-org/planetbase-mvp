2bc4f783f89dbc458666f96cdeec88750c8a6c68
Heytemisan <temyicy@gmail.com>
2022-08-25 11:21:35 +0100

    feat: login state

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
 import Layout from '../../layouts/login-registration';
+import { useState, useEffect } from "react";
 
 function Login() {
+
+    const [formData, setFormData] = useState({
+        fname: '',
+        lname: '',
+        email: '',
+        password: '',
+        password2: '',
+        checkbox: false,
+    });
+
+    const {fname,lname,email, password, password2, checkbox} = formData;
+
+    function onChange(e) {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value,
+        }))
+    };
+
+    function onSubmit(e) {
+        e.preventDefault();
+    }
     return (
         <Layout>
             <div className='input-container'>
