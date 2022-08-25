782ce8ea3ac7e4459881914a9ba3dd117a79cf02
Heytemisan <temyicy@gmail.com>
2022-08-25 11:38:41 +0100

    feat: created authSlice reducer

--- src/redux/authSlice.js
+++ src/redux/authSlice.js
@@ -1,1 +1,30 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+//Get user data from local storage
+const user = JSON.parse(localStorage.getItem('user'));
+
+const initialState = {
+    user: null ? user : null,
+    isError: false,
+    isLoading: false,
+    message: ''
+}
+
+export const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {
+        reset: (state) => {
+            state.isLoading = false;
+            state.isSuccess = false;
+            state.isError = false;
+            state.message = '';
+        }
+    },
+    extraReducers: () => {
+        
+    }
+})
+
+export const { reset } = authSlice.actions;
+export default authSlice.reducer;
