52111fafec84d4829942ca37d53dd648013d6f3c
Heytemisan <temyicy@gmail.com>
2022-08-28 11:08:52 +0100

    feat: created planetbase youtube video section

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import "./Video.css";
 
 function Video() {
   return (
-    <div>Video Section</div>
-  )
+    <div className="youtube-video-container">
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/t9ZZymyrXKQ"
+        frameborder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+    </div>
+  );
 }
 
-export default Video
+export default Video;
--- src/redux/eventSlice.js
+++ src/redux/eventSlice.js
@@ -1,37 +1,37 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-// const initialState = {
-//   title: "",
-//   categories: [],
-//   event: "",
-//   guest: 0,
-//   date: "",
-//   status1: false,
-//   status2: false,
-//   price: 0,
-//   description: "",
-//   sponsorship: "",
-// };
+const initialState = {
+  title: "",
+  categories: [],
+  event: "",
+  guest: 0,
+  date: "",
+  status1: false,
+  status2: false,
+  price: 0,
+  description: "",
+  sponsorship: "",
+};
 
 const eventSlice = createSlice({
   name: "event",
-  initialState:[],
+  initialState,
   reducers: {
-    saveEvent: ((state={ event: [] }, action) => {
+    saveEvent: (state, action) => {
       const newEvent = {
         id: new Date(),
-        title : action.payload.event,
-        categories : action.payload.event,
-        event : action.payload.event,
+        title : action.payload,
+        categories : action.payload,
+        event : action.payload,
         guest : +1,
-        date : action.payload.event,
-        status1 : action.payload.event,
-        status2 : action.payload.event,
+        date : action.payload,
+        status1 : action.payload,
+        status2 : action.payload,
         price : +1,
-        description : action.payload.event,
-        sponsorship : action.payload.event,
+        description : action.payload,
+        sponsorship : action.payload,
       }
-      state.push(newEvent);
+      state.push
     },
     deleteTask: (state, action) => {
       return state.filter((event) => event.id !== action.payload.id)
@@ -39,5 +39,5 @@ const eventSlice = createSlice({
   },
 });
 
-export const { saveEvent, deleteTask } = eventSlice.actions;
+export const { saveEvent } = eventSlice.actions;
 export default eventSlice.reducer;
