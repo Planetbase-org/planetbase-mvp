9c69322d35683b80b2748e88f98c4f63c219c05c
Heytemisan <temyicy@gmail.com>
2022-09-02 17:16:16 +0100

    chore: code refactored

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,17 +1,10 @@
-import React, { useEffect, useState, useTransition } from "react";
+import React, { useEffect } from "react";
 import EventLayout from "../../layouts/events-layout";
 import { FiSearch } from "react-icons/all";
 import "./style.css";
-import { Link } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { saveEvent } from "../../redux/eventSlice";
-import { useNavigate } from "react-router-dom";
-
+import { Link, useNavigate } from "react-router-dom";
 function EventProfile() {
-  const dispatch = useDispatch();
 
-  const { event } = useSelector((state) => state.event);
-  const { user } = useSelector((state) => state.auth);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -19,17 +12,14 @@ function EventProfile() {
     if (!token) {
       navigate("/login");
     }
-    dispatch(saveEvent(event));
   }, []);
 
-  // to check to see if a vendor has created a profile page
-  console.log(useSelector((store) => console.log(store)));
 
   return (
     <EventLayout>
       <div className="event-container">
         <br />
-        <h2>{user.message.firstname || "User"}'s Organization Events</h2>
+        <h2>{ "User"}'s Organization Events</h2>
         <div className="event-input">
           <div className="search-events">
             <FiSearch />
@@ -46,19 +36,19 @@ function EventProfile() {
           <div>
             <h3>Events</h3>
             <div>
-              <p>{event?.title || "No Update"}</p>
+              <p>{"No Update"}</p>
             </div>
           </div>
           <div>
             <h3>Date</h3>
             <div>
-              <p>{event?.date || "No Date"}</p>
+              <p>{"No Date"}</p>
             </div>
           </div>
           <div>
             <h3>Status</h3>
             <div>
-              <p>{event?.categories || "No Status"}</p>
+              <p>{"No Status"}</p>
             </div>
           </div>
         </div>
