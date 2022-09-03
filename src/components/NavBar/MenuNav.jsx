e520239f8367dc4d64a5f8ad92473e3b1827c0a6
Daniel Toba <danieloloruntoba681@gmail.com>
2022-09-03 12:28:58 +0100

    fixed status change bug in the navbar

--- src/components/NavBar/MenuNav.jsx
+++ src/components/NavBar/MenuNav.jsx
@@ -1,98 +1,105 @@
-import React, { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import './Navbar.css'
-import Planetbase from '../../assets/planetbase.png';
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "./Navbar.css";
+import Planetbase from "../../assets/planetbase.png";
 
-function MenuNav () {
+function MenuNav() {
   const [isNavExpanded, setIsNavExpanded] = useState(false);
-  const user = localStorage.getItem("firstname");
+  const user = localStorage.getItem("token");
+  const navigate = useNavigate();
+  const onLogout = () => {
+    localStorage.clear();
+    navigate("/");
+  };
 
   return (
-    <nav className='navigation'>
-      <Link to='/' className='brand-name'>
-        <img src={Planetbase} alt='planetbase' />
+    <nav className="navigation">
+      <Link to="/" className="brand-name">
+        <img src={Planetbase} alt="planetbase" />
       </Link>
       <button
-        className='hamburger'
+        className="hamburger"
         onClick={() => {
-          setIsNavExpanded(!isNavExpanded)
+          setIsNavExpanded(!isNavExpanded);
         }}
       >
         <svg
-          xmlns='http://www.w3.org/2000/svg'
-          className='h-5 w-5'
-          viewBox='0 0 20 20'
-          fill='white'
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          viewBox="0 0 20 20"
+          fill="white"
         >
           <path
-            fillRule='evenodd'
-            d='M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z'
-            clipRule='evenodd'
+            fillRule="evenodd"
+            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+            clipRule="evenodd"
           />
         </svg>
       </button>
       <div
         className={
-          isNavExpanded ? 'navigation-menu expanded' : 'navigation-menu'
+          isNavExpanded ? "navigation-menu expanded" : "navigation-menu"
         }
       >
         <ul>
           <li>
-            <Link to='/products' activeclassname='active'>
+            <Link to="/products" activeclassname="active">
               Product
             </Link>
           </li>
           <li>
-            <Link to='/solutions' activeclassname='active'>
+            <Link to="/solutions" activeclassname="active">
               Solutions
             </Link>
           </li>
           <li>
-            <Link to='#' activeclassname='active'>
+            <Link to="#" activeclassname="active">
               Resources
             </Link>
           </li>
           <li>
-            <Link to='#' activeclassname='active'>
+            <Link to="#" activeclassname="active">
               Company
             </Link>
           </li>
           <div>
-            <li className='login'>
+            <li className="login">
               {user ? (
-                <Link to='/login' activeclassname='active'>
-                Logout
-                </Link>
+                <button activeclassname="active" onClick={onLogout}>
+                  Logout
+                </button>
               ) : (
-                <Link to='/login' activeclassname='active'>
-                Login
+                <Link to="/" activeclassname="active">
+                  Login
                 </Link>
               )}
             </li>
             <li>
-              <button className='loginBtnMobile'>Create Events</button>
+              <button className="loginBtnMobile">Create Events</button>
             </li>
           </div>
         </ul>
-        <div className='createEvents'>
+        <div className="createEvents">
           <li>
             {user ? (
-              <Link to='/login' activeclassname='active'  onClick={onLogout}>
-              Logout
-            </Link>
+              <button activeClassName="active" onClick={onLogout}>
+                Logout
+              </button>
             ) : (
-              <Link to='/login' activeclassname='active'>
-              Login
-            </Link>
+              <Link to="/" activeClassName="active">
+                Login
+              </Link>
             )}
           </li>
           <Link to="/create-event">
-            <button className='loginButton' type="button">Create Events</button>
+            <button className="loginButton" type="button">
+              Create Events
+            </button>
           </Link>
         </div>
       </div>
     </nav>
-  )
+  );
 }
 
-export default MenuNav
+export default MenuNav;
