c4d9200068be8163e94a6941898c0be05c8ebd21
Heytemisan <temyicy@gmail.com>
2022-09-03 18:26:38 +0100

    feat: imported images

--- src/components/ResourceComponent/ResourceComponent.jsx
+++ src/components/ResourceComponent/ResourceComponent.jsx
@@ -1,48 +1,48 @@
-import React from 'react'
-import DetailsCard from '../DetailsCard/DetailsCard';
-import Product1 from "../../assets/product-icon1.svg";
-import Product2 from "../../assets/product-icon2.svg";
-import Product3 from "../../assets/product-icon3.svg";
-import Layout from '../../layouts/products-layout';
+import React from "react";
+import DetailsCard from "../DetailsCard/DetailsCard";
+import Resource1 from "../../assets/resources1.svg";
+import Resource2 from "../../assets/resources2.svg";
+import Resource3 from "../../assets/resources3.svg";
+import Layout from "../../layouts/products-layout";
 
 function ResourceComponent() {
   return (
     <Layout>
-        <div className="product-section">
+      <div className="product-section">
         <div className="product">
           <h4>What is the latest</h4>
           <div className="product-card">
-            <h3>Monthly Brand Update</h3>
+            <h3>2022 Event Planning Toolkit</h3>
             <p>
-              Read About Planetbase brand updates that help with audience
-              engagement, data and insights, production quality, hybrid solution
-              and more.....
+              download our Event Budget, event Brief, and Run of show templates
+              to make planning your in-person, hybrid and virtual events easy.
+              save time, stay organized and feel confident you and your team are
+              on the right track.
             </p>
           </div>
-          <h4>Agency</h4>
-          <DetailsCard
-            img={Product1}
-            header="Case Study"
-            description="See How Planet Base helps Teams achieve their even goals"
-          />
         </div>
         <hr className="line" />
         <div className="product-details">
-          <h4>Product</h4>
+          <h4>Resources</h4>
           <DetailsCard
-            img={Product2}
+            img={Resource1}
+            header="Case Study"
+            description="See How Planet Base helps Teams achieve their even goals"
+          />
+          <DetailsCard
+            img={Resource2}
             header="Blog"
             description="Learn the latest practices &  stay ahead of the industry"
           />
           <DetailsCard
-            img={Product3}
+            img={Resource3}
             header="Support"
             description="Need a hand? lets get your answers to your questions"
           />
         </div>
       </div>
     </Layout>
-  )
+  );
 }
 
-export default ResourceComponent
+export default ResourceComponent;
