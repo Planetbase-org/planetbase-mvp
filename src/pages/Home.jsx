a123ba5cd42a7d0321ef63f2f9f99fb7d5aa92c3
Heytemisan <temyicy@gmail.com>
2022-09-03 16:05:38 +0100

    feat: product ui

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import HeroSection from '../components/HeroSection/Hero';
-import Layout from '../layouts/landing-section';
-import Subscribe from '../components/Subscribe/Subscribe';
-import ModalVideo from '../components/Video/Video';
+import React from "react";
+import HeroSection from "../components/HeroSection/Hero";
+import Layout from "../layouts/landing-section";
+import Subscribe from "../components/Subscribe/Subscribe";
+import ModalVideo from "../components/Video/Video";
 
 function Home() {
-    return (
-        <Layout>
-            <HeroSection />
-                <ModalVideo />
-            <Subscribe/>
-        </Layout>
-    )
+  return (
+    <Layout>
+      <HeroSection />
+      <ModalVideo />
+      <Subscribe />
+    </Layout>
+  );
 }
 
-export default Home
+export default Home;
