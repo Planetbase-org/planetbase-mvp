877660a9f29cc89eae95e5bee90d516ec5eb709a
litmus-zhang <abdulsalamlukmon9@gmail.com>
2022-09-03 18:30:52 +0100

    Fixed Get all events Endpoint

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,9 +11,3 @@ export { default as SponsorBid } from './sponsor/sponsor-bid/sponsor-bid';
 export { default as Payment } from './payment/index';
 export { default as EventProfile } from './dashboard/index';
 export { default as EditEvent } from './dashboard/edit';
-<<<<<<< HEAD
-=======
-export { default as Resources} from './resources/index';
-export { default as Company} from './company/index';
-
->>>>>>> 850ddd20e8725229a821fa3086444a2a4386ca8e
