c5b6c96da46545518d605aeb8013a247c8a945be
Heytemisan <temyicy@gmail.com>
2022-09-07 20:23:11 +0100

    chore: adjusted subscribers form

--- src/components/Subscribe/Subscribe.jsx
+++ src/components/Subscribe/Subscribe.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import Planetbase from '../../assets/planetbase.png';
+import React from "react";
+import Planetbase from "../../assets/planetbase.png";
 import "./Subscribe.css";
 
 function Subscribe() {
@@ -10,19 +10,39 @@ function Subscribe() {
       </div>
       <div className="subscribe-message">
         <div>
-          <h4>Moving your events online really opens up the esteem of supporting it.</h4>
-          <p>Become a subscriber and receive great tips on event regulation updates sent to your inbox!.</p>
+          <h4>
+            Moving your events online really opens up the esteem of supporting
+            it.
+          </h4>
+          <p>
+            Become a subscriber and receive great tips on event regulation
+            updates sent to your inbox!.
+          </p>
         </div>
         <div>
-          <form className="subscribe-form">
-            <input type="text" placeholder='Enter email address' className='subscribe-email-input'/>
-            <button className='subscribe-btn'>Subscribe</button>
-          </form>
-          <p className='subscribe-message-text'>By signing up, you agree to our Terms of Service and Privacy Policy. You will receive offers from Wimdu and agree that we may share your hashed email address with third parties for the purpose of better tailoring advertising to your needs.</p>
+          <div class="parent-wrapper">
+            <span class="close-btn glyphicon glyphicon-remove"></span>
+            <div class="subscribe-wrapper">
+              <h4>SUBSCRIBE TO OUR NEWSLETTER</h4>
+              <input
+                type="email"
+                name="email"
+                class="subscribe-input"
+                placeholder="Your e-mail"
+              />
+              <div class="submit-btn">SUBMIT</div>
+            </div>
+          </div>
+          {/* <p className="subscribe-message-text">
+            By signing up, you agree to our Terms of Service and Privacy Policy.
+            You will receive offers from Wimdu and agree that we may share your
+            hashed email address with third parties for the purpose of better
+            tailoring advertising to your needs.
+          </p> */}
         </div>
       </div>
     </section>
-  )
+  );
 }
 
-export default Subscribe
+export default Subscribe;
